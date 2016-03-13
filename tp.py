f4b86432130f82ae026b7f514681873400c3d406
mjws00 <mjws00@shaw.ca>
2016-03-12 21:04:55 -0800

    test from mac

--- tp.py
+++ tp.py
@@ -2,7 +2,7 @@ fin = open('words.txt')
 count = 0
 for line in fin:
     word = line.strip()
-    if len(word) > 17:
+    if len(word) > 9:
         count += 1
         print(word)
 print("The count was:", count)
